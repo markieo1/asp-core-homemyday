61fc17d8b68f6f71096073044e284768cdb4f5c6
Hugo Zink <hugo.zink@hotmail.com>
2017-09-19 12:46:45 +0200

    Added [Key] annotations to ID properties of Holiday and Accommodation.

---
+++ HomeMyDay/Migrations/20170919104521_PrimaryKeys.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace HomeMyDay.Migrations
+{
+    public partial class PrimaryKeys : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
