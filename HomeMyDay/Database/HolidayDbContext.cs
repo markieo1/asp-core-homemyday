c86af14012fe6e64b9940f1403d28ff3b50cd258
Hugo Zink <hugo.zink@hotmail.com>
2017-09-19 12:35:35 +0200

    Added model data annotations, added migrations.

--- HomeMyDay/Database/HolidayDbContext.cs
+++ HomeMyDay/Database/HolidayDbContext.cs
@@ -7,8 +7,17 @@ using System.Threading.Tasks;
 
 namespace HomeMyDay.Database
 {
-    public class HolidayDbContext : DbContext
-    {
+	public class HolidayDbContext : DbContext
+	{
+		/// <summary>
+		/// Required constructor for entity framework. Passes options to parent class constructor.
+		/// </summary>
+		/// <param name="options">The DB options to pass to the base class.</param>
+		public HolidayDbContext(DbContextOptions<HolidayDbContext> options) : base(options)
+		{
+
+		}
+
 		/// <summary>
 		/// The Holidays that have been saved.
 		/// </summary>
@@ -18,5 +27,5 @@ namespace HomeMyDay.Database
 		/// The Accommodations that have been saved.
 		/// </summary>
 		public DbSet<Accommodation> Accommodations { get; set; }
-    }
+	}
 }
---
+++ HomeMyDay/Migrations/20170919102647_InitialCreate.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace HomeMyDay.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Accommodations",
+                columns: table => new
+                {
+                    AccommodationID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accommodations", x => x.AccommodationID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Holidays",
+                columns: table => new
+                {
+                    HolidayID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    AccommodationID = table.Column<int>(type: "int", nullable: true),
+                    DepartureDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    NrPersons = table.Column<int>(type: "int", nullable: false),
+                    ReturnDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Holidays", x => x.HolidayID);
+                    table.ForeignKey(
+                        name: "FK_Holidays_Accommodations_AccommodationID",
+                        column: x => x.AccommodationID,
+                        principalTable: "Accommodations",
+                        principalColumn: "AccommodationID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Holidays_AccommodationID",
+                table: "Holidays",
+                column: "AccommodationID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Holidays");
+
+            migrationBuilder.DropTable(
+                name: "Accommodations");
+        }
+    }
+}
