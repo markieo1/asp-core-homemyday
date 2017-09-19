acf3e14fe7b6110961d377812283d633aedc796b
W10-RITCHIE\Ritchie <richard.bos93@gmail.com>
2017-09-19 15:01:26 +0200

    Changes to Booking and Holiday Domain Classes

---
+++ HomeMyDay/Models/Holiday.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeMyDay.Models.Domain
+{
+    public class Holiday
+    {
+        /// <summary>
+        /// Key for the Database 
+        /// </summary>
+        [Key]
+        public int HolidayId { get; set; }
+
+        /// <summary>
+        /// The destination where the customer is going
+        /// </summary>
+        public string Destination { get; set; }
+
+        /// <summary>
+        /// The category is for multiple choices like: houses, experiences, places
+        /// </summary>
+        public string Category { get; set; }
+
+        /// <summary>
+        /// Recommended can be set true or false to see if a holiday is recommended for the user
+        /// </summary>
+        public bool Recommended { get; set; }
+
+        /// <summary>
+        /// Optional: The amount of rooms available during the holiday
+        /// </summary>
+        public int Room { get; set; }
+
+        /// <summary>
+        /// Optional: The amount of beds available in total of all the rooms
+        /// </summary>
+        public int Bed { get; set; }
+
+        /// <summary>
+        /// The price for the holiday
+        /// </summary>
+        public decimal Price { get; set; }
+    }
+}
