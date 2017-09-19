82cf1fa09b673b2eff9ed8edcec93771bc14ef9c
Hugo Zink <hugo.zink@hotmail.com>
2017-09-19 12:07:30 +0200

    Added the DB context class for the Holidays and Accommodations.

---
+++ HomeMyDay/Database/HolidayDbContext.cs
@@ -0,0 +1,22 @@
+﻿using HomeMyDay.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeMyDay.Database
+{
+    public class HolidayDbContext : DbContext
+    {
+		/// <summary>
+		/// The Holidays that have been saved.
+		/// </summary>
+		public DbSet<Holiday> Holidays { get; set; }
+		
+		/// <summary>
+		/// The Accommodations that have been saved.
+		/// </summary>
+		public DbSet<Accommodation> Accommodations { get; set; }
+    }
+}
