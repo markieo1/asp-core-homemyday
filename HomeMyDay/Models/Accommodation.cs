b59bd96d2c5582a0b3df80e3adb583647c8d81bd
Hugo Zink <hugo.zink@hotmail.com>
2017-09-19 11:46:47 +0200

    Added Holiday and Accommodation entities.

---
+++ HomeMyDay/Models/Accommodation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeMyDay.Models
+{
+    public class Accommodation
+    {
+		public string Name { get; set; }
+    }
+}
---
+++ HomeMyDay/Models/Holiday.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeMyDay.Models
+{
+    public class Holiday
+    {
+		public Accommodation Accommodation { get; set; }
+		public DateTime DepartureDate { get; set; }
+		public DateTime ReturnDate { get; set; }
+		public int NrPersons { get; set; }
+    }
+}
