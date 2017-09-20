cc7907486527f4255c52284be1bcc221a5657fb1
Kevin Ly <kevinly92@hotmai.com>
2017-09-20 13:26:15 +0200

    work in progress

---
+++ HomeMyDay/TagHelpers/MenuTagHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace HomeMyDay.TagHelpers
+{
+	[HtmlTargetElement(Attributes = "menu-link")]
+    public class MenuTagHelper : TagHelper
+    {
+	    public override void Process(TagHelperContext context, TagHelperOutput output)
+	    {
+			base.Process(context, output);
+			var classAttr = new TagHelperAttribute("class", "active");
+		    output.Attributes.Add(classAttr);
+			//output.Attributes.SetAttribute("class", "active");
+	    }
+    }
+}
