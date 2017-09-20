386d053fd061e82b012fd0d48838aefd64af6510
W10-RITCHIE\Ritchie <richard.bos93@gmail.com>
2017-09-20 10:53:50 +0200

    Holiday feature not done yet

---
+++ HomeMyDay/Components/HolidayComponent.cs
@@ -0,0 +1,49 @@
+﻿using HomeMyDay.Models.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeMyDay.Components
+{
+    public class HolidayComponent : ViewComponent
+    {
+        private IEnumerable<Holiday> model;
+        private IRepositoryHoliday repository;
+        public string Destination { get; set; }
+
+        class Holiday
+        {
+            public string Destination { get; set; }
+            public string Place { get; set; }
+            public bool Recommended { get; set; }
+            public int Price { get; set; }
+        }
+
+        private List<Holiday> fakeHolidays = new List<Holiday>
+        {
+            new Holiday {Destination = "Netherlands", Place = "Rotterdam", Recommended = true, Price = 128},
+            new Holiday {Destination = "Netherlands", Place = "Amsterdam", Recommended = false, Price = 148},
+        };
+
+        public HolidayComponent(IRepositoryHoliday repository)
+        {
+            this.repository = repository;
+        }
+
+        public IViewComponentResult ShowRecommendedHoliday(int amount)
+        {
+            model = (IEnumerable<Holiday>) fakeHolidays;
+
+            model.Where(m => m.Recommended == true);
+
+            foreach (var m in model)
+            {
+                Destination = m.Destination;
+            };
+
+            return View("RecommendedHoliday", this);
+        }
+    }
+}
---
+++ HomeMyDay/Models/Interfaces/IRepositoryHoliday.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeMyDay.Models.Interfaces
+{
+    public interface IRepositoryHoliday
+    {
+        IEnumerable<Holiday> Holidays { get; }
+        void AddHoliday();
+    }
+}
