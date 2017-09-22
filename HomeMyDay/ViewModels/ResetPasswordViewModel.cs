29f1402f59a4450068ae668e9f6eb322b7e80d62
Kevin Ly <kevinly92@hotmai.com>
2017-09-22 10:54:49 +0200

    Add viewmodel to view

---
+++ HomeMyDay/ViewModels/ResetPasswordViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace HomeMyDay.ViewModels
+{
+    public class ResetPasswordViewModel
+    {
+		/// <summary>
+		/// Gets or sets the Id of the user.
+		/// </summary>
+		[Required]
+		public Guid Id { get; set; }
+
+		/// <summary>
+		/// Gets or sets the new password of the user.
+		/// </summary>
+		[DisplayName("Nieuw wachtwoord")]
+		[Required(ErrorMessage = "Vul een nieuw wachtwoord in.")]
+		public string NewPassword { get; set; }
+
+		/// <summary>
+		/// Gets or sets the new password confirmation of the user.
+		/// </summary>
+		[DisplayName("Herhaal nieuw wachtwoord")]
+		[Required(ErrorMessage = "Het wachtwoord komt niet overeen met het nieuwe wachtwoord.")]
+		public string NewPasswordConfirmation { get; set; }
+    }
+}
