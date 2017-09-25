56eee96fc8e12ee35c2de312deaf9f315ccc3649
Kevin Ly <kevinly92@hotmai.com>
2017-09-25 09:26:31 +0200

    added comments

--- HomeMyDay/ViewModels/AccountViewModel.cs
+++ HomeMyDay/ViewModels/AccountViewModel.cs
@@ -4,8 +4,14 @@ namespace HomeMyDay.ViewModels
 {
     public class AccountViewModel
     {
+		/// <summary>
+		/// The Id of the user.
+		/// </summary>
 		public Guid Id { get; set; }
 
+		/// <summary>
+		/// The name of the user.
+		/// </summary>
 		public string Username { get; set; }
     }
 }
