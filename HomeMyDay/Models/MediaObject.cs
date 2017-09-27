ae7092f2bbcc4f9aaea9c8ab2ef961a6acb769dc
Marco <marcohavermans+github@hotmail.nl>
2017-09-27 10:42:41 +0200

    Added mediaobject for accommodation. Added method to repo to get one accommodatin.

---
+++ HomeMyDay/Models/MediaObject.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeMyDay.Models
+{
+	public class MediaObject
+	{
+		/// <summary>
+		/// Gets or sets the identifier.
+		/// </summary>
+		[Key]
+		public long Id { get; set; }
+
+		/// <summary>
+		/// Gets or sets the URL of the media.
+		/// </summary>
+		public string Url { get; set; }
+
+		/// <summary>
+		/// Gets or sets the type of this media object.
+		/// </summary>
+		public MediaType Type { get; set; }
+
+		/// <summary>
+		/// Gets or sets the description.
+		/// </summary>
+		public string Description { get; set; }
+	}
+}
---
+++ HomeMyDay/Models/MediaType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeMyDay.Models
+{
+	public enum MediaType
+	{
+		Unknown = 0,
+		Image = 1,
+		Video = 2
+	}
+}
