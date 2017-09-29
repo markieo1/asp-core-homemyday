eeaf7ee0628e5726a7802769902a752d704caab4
Daniel van Bavel <dvanbavel@gmail.com>
2017-09-29 12:24:46 +0200

    edit Comments

--- HomeMyDay/Repository/IvacancieRepository.cs
+++ HomeMyDay/Repository/IvacancieRepository.cs
@@ -5,8 +5,19 @@ namespace HomeMyDay.Repository
 {
     public interface IVacancieRepository
     {
+        /// <summary>
+		/// Gets the Vacancies.
+		/// </summary>
+		/// <returns>IEnumerable containing all vacancies</returns>
         IEnumerable<Vacancie> GetVacancies { get; }
 
+        /// <summary>
+        /// Gets one vacancie.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentOutOfRangeException">id</exception>
+        /// <exception cref="KeyNotFoundException"></exception>
         Vacancie GetVacancie(long id);
     }
 }
