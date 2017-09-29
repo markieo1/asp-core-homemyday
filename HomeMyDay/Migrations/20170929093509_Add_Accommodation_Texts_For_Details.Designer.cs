267e5c4a7bc796162491e6e35628672c8be8234a
Marco <marcohavermans+github@hotmail.nl>
2017-09-29 11:36:38 +0200

    Created migration to add detail texts for accommodation.

--- HomeMyDay/Migrations/20170929093509_Add_Accommodation_Texts_For_Details.Designer.cs
+++ HomeMyDay/Migrations/20170929093509_Add_Accommodation_Texts_For_Details.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace HomeMyDay.Migrations
 {
     [DbContext(typeof(HomeMyDayDbContext))]
-    partial class HolidayDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170929093509_Add_Accommodation_Texts_For_Details")]
+    partial class Add_Accommodation_Texts_For_Details
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
