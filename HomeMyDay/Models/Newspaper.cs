dd344d430c702f9f3ef71890136cf46ec112f8b2
Kevin Ly <kevinly92@hotmai.com>
2017-09-29 13:54:29 +0200

    Add required and emailadress attribute

--- HomeMyDay/Models/Newspaper.cs
+++ HomeMyDay/Models/Newspaper.cs
@@ -1,10 +1,14 @@
-﻿namespace HomeMyDay.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HomeMyDay.Models
 {
 	public class Newspaper : BaseModel
     {
 		/// <summary>
 		/// The email of the user
 		/// </summary>
+		[Required]
+		[EmailAddress]
 		public string Email { get; set; }
     }
 }
