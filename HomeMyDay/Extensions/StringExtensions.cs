0eee7ad9cc6480f2b21df026f62fc4a55494e1c4
Marco <marcohavermans+github@hotmail.nl>
2017-09-29 10:01:00 +0200

    Created extension method to truncate string. Made detail page include location and country.

--- HomeMyDay/Extensions/StringExtensions.cs
+++ HomeMyDay/Extensions/StringExtensions.cs
@@ -16,5 +16,26 @@ namespace HomeMyDay.Extensions
 		{
 			return str.Replace("Controller", "");
 		}
+
+		/// <summary>
+		/// Truncates the string to the specified amount of characters.
+		/// </summary>
+		/// <param name="value">The string.</param>
+		/// <param name="maxLength">The max length of the string.</param>
+		/// <returns></returns>
+		public static string Truncate(this string value, int maxLength)
+		{
+			if (string.IsNullOrEmpty(value))
+				return value;
+
+			if (value.Length <= maxLength)
+			{
+				return value;
+			}
+			else
+			{
+				return $"{value.Substring(0, maxLength)}...";
+			}
+		}
 	}
 }
