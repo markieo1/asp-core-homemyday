0ad02d271a812224eea352799ba70ed73ffae194
Hugo Zink <hugo.zink@hotmail.com>
2017-09-29 14:13:15 +0200

    Added required attributes to all properties in BookingPerson.

--- HomeMyDay/Models/BookingPerson.cs
+++ HomeMyDay/Models/BookingPerson.cs
@@ -19,60 +19,70 @@ namespace HomeMyDay.Models
 		/// The salutation of the person (mr/mrs)
 		/// </summary>
 		[DisplayName("Aanhef")]
+		[Required]
 		public string Salutation { get; set; }
 
 		/// <summary>
 		/// The person's initials.
 		/// </summary>
 		[DisplayName("Voorletters")]
+		[Required]
 		public string Initials { get; set; }
 
 		/// <summary>
 		/// The person's first name.
 		/// </summary>
 		[DisplayName("Voornaam")]
+		[Required]
 		public string FirstName { get; set; }
 
 		/// <summary>
 		/// The person's insertion (tussenvoegsel).
 		/// </summary>
 		[DisplayName("Tussenvoegsel")]
+		[Required]
 		public string Insertion { get; set; }
 
 		/// <summary>
 		/// The person's last name.
 		/// </summary>
 		[DisplayName("Achternaam")]
+		[Required]
 		public string LastName { get; set; }
 
 		/// <summary>
 		/// The date the person was born.
 		/// </summary>
 		[DisplayName("Geboortedatum")]
+		[Required]
 		public DateTime BirthDate { get; set; }
 
 		/// <summary>
 		/// The person's email address.
 		/// </summary>
 		[DisplayName("E-mailadres")]
+		[Required]
 		public string Email { get; set; }
 
 		/// <summary>
 		/// The country the person resides in.
 		/// </summary>
 		[DisplayName("Land")]
+		[Required]
 		public Country Country { get; set; }
 
 		/// <summary>
 		/// The postal code that the person resides in.
 		/// </summary>
 		[DisplayName("Postcode")]
+		[Required]
 		public string PostalCode { get; set; }
 
 		/// <summary>
 		/// The house number of the person.
 		/// </summary>
 		[DisplayName("Huisnummer")]
+		[Required]
 		public int HouseNumber { get; set; }
 
 		/// <summary>
@@ -85,12 +95,14 @@ namespace HomeMyDay.Models
 		/// The person's phone number.
 		/// </summary>
 		[DisplayName("Telefoonnummer")]
+		[Required]
 		public string PhoneNumber { get; set; }
 
 		/// <summary>
 		/// The country that the person is from.
 		/// </summary>
 		[DisplayName("Nationaliteit")]
+		[Required]
 		public Country Nationality { get; set; }
 
 		/// <summary>
