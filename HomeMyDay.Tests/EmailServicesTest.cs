0c1596cfba3755a976ed41676892b3e3483c9f49
Danny Tam <kwd.tam@student.avans.nl>
2017-10-02 12:07:16 +0200

    Wip

---
+++ HomeMyDay.Tests/EmailServicesTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace HomeMyDay.Tests
+{
+	//unit test with email
+	//unit test test email
+	class EmailServicesTest
+	{
+		[Fact]
+		public void TestEmailConfirmed()
+		{
+
+		}
+
+		[Fact]
+		public void TestEmailCreated()
+		{
+
+		}
+
+		[Fact]
+		public void TestEmailVertificationCode()
+		{
+
+		}
+
+		[Fact]
+		public void TestEmailPasswordChanged()
+		{
+
+		}
+	}
+}
