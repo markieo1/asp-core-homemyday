4fbcc320fb119facff7acf75a9295c150d16c068
Daniel van Bavel <dvanbavel@gmail.com>
2017-10-03 11:57:29 +0200

    setup faq

---
+++ HomeMyDay/Models/FaqQuestions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeMyDay.Models
+{
+    public class FaqQuestions
+    {
+    }
+}
