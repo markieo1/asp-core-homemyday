f46413f036162f38f33f05fa5ede24102a040e5e
Kevin Ly <kevinly92@hotmai.com>
2017-10-06 11:24:58 +0200

    Added Cms, tests in progress

---
+++ HomeMyDay.Tests/EFFaqRepositoryTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using HomeMyDay.Database;
+using HomeMyDay.Repository;
+using HomeMyDay.Repository.Implementation;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace HomeMyDay.Tests
+{
+	public class EFFaqRepositoryTest
+    {
+	    [Fact]
+	    public void TestFaqEmptyRepository()
+	    {
+		    var optionsBuilder = new DbContextOptionsBuilder<HomeMyDayDbContext>();
+		    optionsBuilder.UseInMemoryDatabase(Guid.NewGuid().ToString());
+		    HomeMyDayDbContext context = new HomeMyDayDbContext(optionsBuilder.Options);
+		    IFaqRepository repository = new EFFaqRepository(context);
+
+		    Assert.Empty(repository.GetCategoriesAndQuestions());
+		}
+
+	    [Fact]
+	    public void TestFaqFilledRepository()
+	    {
+		    
+	    }
+
+	    [Fact]
+	    public void TestFaqCmsEmptyList()
+	    {
+		    
+	    }
+
+	    [Fact]
+	    public void TestFaqCmsFilledList()
+	    {
+		    
+	    }
+
+	    [Fact]
+	    public void TestFaqCmsPageArgumentOutOfRangeException()
+	    {
+		    
+	    }
+
+	    [Fact]
+	    public void TestFaqCmsPageSizeArgumentOutOfRangeException()
+	    {
+		    
+	    }
+    }
+}
--- HomeMyDay/Repository/Implementation/EFFaqRepository.cs
+++ HomeMyDay/Repository/Implementation/EFFaqRepository.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HomeMyDay.Helpers;
 
 namespace HomeMyDay.Repository.Implementation
 {
@@ -21,5 +22,22 @@ namespace HomeMyDay.Repository.Implementation
         {
             return _context.FaqCategory.Include(nameof(FaqCategory.FaqQuestions));
         }
+
+	    public Task<PaginatedList<FaqCategory>> List(int page = 1, int pageSize = 10)
+	    {
+		    if (page < 1)
+		    {
+			    throw new ArgumentOutOfRangeException();
+		    }
+
+		    if (pageSize < 1)
+		    {
+			    throw new ArgumentOutOfRangeException();
+		    }
+
+		    var faqCategories = _context.FaqCategory.OrderBy(x => x.Id).AsNoTracking();
+
+		    return PaginatedList<FaqCategory>.CreateAsync(faqCategories, page, pageSize);
+	    }
     }
 }
