954364abd6c4e03292ebb9b12ae09497414e160b
Marco <marcohavermans+github@hotmail.nl>
2017-10-06 13:53:25 +0200

    Created seperate page to display questions per category.

--- HomeMyDay/Repository/IFaqRepository.cs
+++ HomeMyDay/Repository/IFaqRepository.cs
@@ -6,12 +6,12 @@ using HomeMyDay.Helpers;
 namespace HomeMyDay.Repository
 {
 	public interface IFaqRepository
-    {
-        /// <summary>
+	{
+		/// <summary>
 		/// Get All Categories and Questions 
 		/// </summary>
 		/// <returns>The categories and the linked questions to a categorie</returns>
-        IEnumerable<FaqCategory> GetCategoriesAndQuestions();
+		IEnumerable<FaqCategory> GetCategoriesAndQuestions();
 
 		/// <summary>
 		/// Lists the FaqCategories for the specific page.
@@ -19,6 +19,22 @@ namespace HomeMyDay.Repository
 		/// <param name="page">The page.</param>
 		/// <param name="pageSize">Size of the page.</param>
 		/// <returns></returns>
-	    Task<PaginatedList<FaqCategory>> List(int page = 1, int pageSize = 10);
-    }
+		Task<PaginatedList<FaqCategory>> ListCategories(int page = 1, int pageSize = 10);
+
+		/// <summary>
+		/// Gets the category.
+		/// </summary>
+		/// <param name="categoryId">The category identifier.</param>
+		/// <returns></returns>
+		FaqCategory GetCategory(long categoryId);
+
+		/// <summary>
+		/// Lists the FaqCategories for the specific page.
+		/// </summary>
+		/// <param name="categoryId">The id of the category</param>
+		/// <param name="page">The page.</param>
+		/// <param name="pageSize">Size of the page.</param>
+		/// <returns></returns>
+		Task<PaginatedList<FaqQuestion>> ListQuestions(long categoryId, int page = 1, int pageSize = 10);
+	}
 }
--- HomeMyDay/Repository/Implementation/EFFaqRepository.cs
+++ HomeMyDay/Repository/Implementation/EFFaqRepository.cs
@@ -9,35 +9,82 @@ using HomeMyDay.Helpers;
 
 namespace HomeMyDay.Repository.Implementation
 {
-    public class EFFaqRepository : IFaqRepository
-    {
-        private readonly HomeMyDayDbContext _context;
+	public class EFFaqRepository : IFaqRepository
+	{
+		private readonly HomeMyDayDbContext _context;
 
-        public EFFaqRepository(HomeMyDayDbContext context)
-        {
-            _context = context;
-        }
+		public EFFaqRepository(HomeMyDayDbContext context)
+		{
+			_context = context;
+		}
 
-        public IEnumerable<FaqCategory> GetCategoriesAndQuestions()
-        {
-            return _context.FaqCategory.Include(nameof(FaqCategory.FaqQuestions));
-        }
+		public IEnumerable<FaqCategory> GetCategoriesAndQuestions()
+		{
+			return _context.FaqCategory.Include(nameof(FaqCategory.FaqQuestions));
+		}
 
-	    public Task<PaginatedList<FaqCategory>> List(int page = 1, int pageSize = 10)
-	    {
-		    if (page < 1)
-		    {
-			    throw new ArgumentOutOfRangeException();
-		    }
+		public FaqCategory GetCategory(long categoryId)
+		{
+			if (categoryId <= 0)
+			{
+				throw new ArgumentOutOfRangeException(nameof(categoryId));
+			}
 
-		    if (pageSize < 1)
-		    {
-			    throw new ArgumentOutOfRangeException();
-		    }
+			FaqCategory category = _context.FaqCategory
+				.FirstOrDefault(a => a.Id == categoryId);
 
-		    var faqCategories = _context.FaqCategory.OrderBy(x => x.Id).AsNoTracking();
+			if (category == null)
+			{
+				throw new KeyNotFoundException($"Category with ID: {categoryId} is not found");
+			}
 
-		    return PaginatedList<FaqCategory>.CreateAsync(faqCategories, page, pageSize);
-	    }
-    }
+			return category;
+		}
+
+		public Task<PaginatedList<FaqCategory>> ListCategories(int page = 1, int pageSize = 10)
+		{
+			// Reset to default value
+			if (pageSize <= 0)
+			{
+				pageSize = 10;
+			}
+
+			// We are not able to skip before the first page
+			if (page <= 0)
+			{
+				page = 1;
+			}
+
+			var faqCategories = _context.FaqCategory.OrderBy(x => x.Id).AsNoTracking();
+
+			return PaginatedList<FaqCategory>.CreateAsync(faqCategories, page, pageSize);
+		}
+
+		public Task<PaginatedList<FaqQuestion>> ListQuestions(long categoryId, int page = 1, int pageSize = 10)
+		{
+			if (categoryId <= 0)
+			{
+				throw new ArgumentOutOfRangeException(nameof(categoryId));
+			}
+
+			// Reset to default value
+			if (pageSize <= 0)
+			{
+				pageSize = 10;
+			}
+
+			// We are not able to skip before the first page
+			if (page <= 0)
+			{
+				page = 1;
+			}
+
+			var faqQuestions = _context.FaqQuestions
+				.Where(x => x.CategoryId == categoryId)
+				.OrderBy(x => x.Id)
+				.AsNoTracking();
+
+			return PaginatedList<FaqQuestion>.CreateAsync(faqQuestions, page, pageSize);
+		}
+	}
 }
