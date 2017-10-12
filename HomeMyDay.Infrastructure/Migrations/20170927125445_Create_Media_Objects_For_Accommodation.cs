62043bb4b99f33b99ded37d8c9809670f98b43ae
Hugo Zink <hugo.zink@hotmail.com>
2017-10-12 17:04:44 +0200

    Moved migrations to Infrastructure, fixed Package References in csproj.

---
+++ HomeMyDay.Infrastructure/Migrations/20170927125445_Create_Media_Objects_For_Accommodation.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace HomeMyDay.Infrastructure.Migrations
+{
+    public partial class Create_Media_Objects_For_Accommodation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MediaObjects",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    AccommodationId = table.Column<int>(type: "int", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    Url = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MediaObjects", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MediaObjects_Accommodations_AccommodationId",
+                        column: x => x.AccommodationId,
+                        principalTable: "Accommodations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MediaObjects_AccommodationId",
+                table: "MediaObjects",
+                column: "AccommodationId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MediaObjects");
+        }
+    }
+}
