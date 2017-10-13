66e9e025ed083b46ccc2c99744e2bdc7e6e77d98
Kevin Ly <kevinly92@hotmai.com>
2017-10-13 17:46:59 +0200

    Removed unused usings

--- HomeMyDay.Web.Site.Home/Controllers/PressController.cs
+++ HomeMyDay.Web.Site.Home/Controllers/PressController.cs
@@ -1,12 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace HomeMyDay.Web.Site.Home.Controllers
 {
-    public class PressController : Controller
+	public class PressController : Controller
     {
         public ViewResult Index()
         {
