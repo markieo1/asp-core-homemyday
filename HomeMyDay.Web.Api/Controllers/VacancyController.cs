55d125b6e137291c1ec4e9587cfb4b78503bf187
Richard Bos <richard.bos09@outlook.com>
2017-10-26 00:38:44 +0200

    Added vacancy

---
+++ HomeMyDay.Web.Api/Controllers/VacancyController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HomeMyDay.Web.Base.Managers;
+using HomeMyDay.Core.Models;
+
+namespace HomeMyDay.Web.Api.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/vacancies")]
+    public class VacancyController : Controller
+    {
+        private readonly IVacancyManager vacancyManager;
+
+        public VacancyController(IVacancyManager vacancyMgr)
+        {
+            vacancyManager = vacancyMgr;
+        }
+
+        // EGT api/values
+        [HttpGet]
+        public IEnumerable<Vacancy> Get()
+        {
+            return vacancyManager.GetVacancies();
+        }
+
+        // GET api/values
+        [HttpGet("{id}")]
+        public Vacancy Get(long id)
+        {
+            return vacancyManager.GetVacancy(id);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post([FromBody]Vacancy vacancy)
+        {
+            vacancyManager.Save(vacancy);
+
+            return CreatedAtAction(nameof(Get), new { id = vacancy.Id }, vacancy);
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody]Vacancy[] vacancies)
+        {
+            foreach (Vacancy vacancy in vacancies)
+            {
+                vacancyManager.Save(vacancy);
+            }
+
+            return Accepted();
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public IActionResult Put(long id, [FromBody]Vacancy vacancy)
+        {
+            if (vacancy.Id != id)
+            {
+                return BadRequest();
+            }
+
+            vacancyManager.Save(vacancy);
+
+            return AcceptedAtAction(nameof(Get), new { id = vacancy.Id }, vacancy);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteAsync()
+        {
+            IEnumerable<Vacancy> vacancies = vacancyManager.GetVacancies();
+
+            foreach (Vacancy vacancy in vacancies)
+            {
+                await vacancyManager.Delete(vacancy.Id);
+            }
+
+            return Accepted();
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            vacancyManager.Delete(id);
+
+            return AcceptedAtAction(nameof(Get));
+        }
+
+    }
+}
