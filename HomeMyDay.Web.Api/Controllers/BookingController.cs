358b60632a06f800a0cd00be1a315b2f24d61307
Danny <kenwai2010@gmail.com>
2017-10-26 01:35:43 +0200

    Booking api init

---
+++ HomeMyDay.Web.Api/Controllers/BookingController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HomeMyDay.Web.Base.Managers;
+using HomeMyDay.Core.Models;
+
+namespace HomeMyDay.Web.Api.Controllers
+{
+	[Produces("application/json")]
+    [Route("api/bookings")]
+    public class BookingController : Controller
+    {
+		//private readonly IBookingManager BookingManager;
+
+		//public BookingController(IBookingManager BookingMgr)
+		//{
+			//BookingyManager = BookingMgr;
+		//}
+
+		[HttpGet]
+		public IEnumerable<Booking> Get()
+		{
+			//return BookingManager.GetCountries();
+
+		}
+
+		// GET api/values
+		[HttpGet("{id}")]
+		public Booking Get(int id)
+        {
+			//return BookingManager.GetBooking(id);
+			return new Booking();
+		}
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post([FromBody]Booking Booking)
+        {
+			//BookingManager.Save(Booking);
+
+			return CreatedAtAction(nameof(Get), new { id = Booking.Id }, Booking);
+        }
+
+		public IActionResult Put([FromBody]Booking[] bookings)
+		{
+			return BadRequest();
+		}
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody]Booking booking)
+        {
+			return BadRequest();
+		}
+
+		[HttpDelete]
+		public IActionResult Delete()
+		{
+			return BadRequest();
+		}
+
+		// DELETE api/values/5
+		[HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+			return BadRequest();
+        }
+    }
+}
