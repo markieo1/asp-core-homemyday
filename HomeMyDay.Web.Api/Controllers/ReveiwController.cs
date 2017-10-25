c58a4c65c1ea173d4ea9595f7aa70809a4ade47c
Richard Bos <richard.bos09@outlook.com>
2017-10-26 00:35:12 +0200

    Vacancy Added

--- HomeMyDay.Web.Api/Controllers/ReveiwController.cs
+++ HomeMyDay.Web.Api/Controllers/ReveiwController.cs
@@ -8,83 +8,83 @@ using HomeMyDay.Core.Models;
 
 namespace HomeMyDay.Web.Api.Controllers
 {
-	[Produces("application/json")]
-    [Route("api/reviews")]
-    public class ReviewController : Controller
-    {
-		private readonly IReviewManager ReviewManager;
+	//[Produces("application/json")]
+ //   [Route("api/reviews")]
+ //   public class ReviewController : Controller
+ //   {
+	//	private readonly IReviewManager ReviewManager;
 
-		public ReviewController(IReviewManager ReviewMgr)
-		{
-			ReviewManager = ReviewMgr;
-		}
+	//	public ReviewController(IReviewManager ReviewMgr)
+	//	{
+	//		ReviewManager = ReviewMgr;
+	//	}
 
-		[HttpGet]
-		public IEnumerable<Review> Get()
-		{
-			return ReviewManager.GetReviews();
-		}
+	//	[HttpGet]
+	//	public IEnumerable<Review> Get()
+	//	{
+	//		return ReviewManager.GetReviews();
+	//	}
 
-		// GET api/values
-		[HttpGet("{id}")]
-		public Review Get(int id)
-        {
-			return ReviewManager.GetReview(id);
-        }
+	//	// GET api/values
+	//	[HttpGet("{id}")]
+	//	public Review Get(int id)
+ //       {
+	//		return ReviewManager.GetReview(id);
+ //       }
 
-        // POST api/values
-        [HttpPost]
-        public IActionResult Post([FromBody]Review Review)
-        {
-			//ReviewManager.(Review);
+ //       // POST api/values
+ //       [HttpPost]
+ //       public IActionResult Post([FromBody]Review Review)
+ //       {
+	//		//ReviewManager.(Review);
 
-			return CreatedAtAction(nameof(Get), new { id = Review.Id }, Review);
-        }
+	//		return CreatedAtAction(nameof(Get), new { id = Review.Id }, Review);
+ //       }
 
-		[HttpPut]
-		public IActionResult Put([FromBody]Review[] countries)
-		{
-			foreach(Review review in countries)
-			{
-				ReviewManager.Save(review);
-			}
+	//	[HttpPut]
+	//	public IActionResult Put([FromBody]Review[] countries)
+	//	{
+	//		foreach(Review review in countries)
+	//		{
+	//			ReviewManager.Save(review);
+	//		}
 
-			return Accepted();
-		}
+	//		return Accepted();
+	//	}
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public IActionResult Put(int id, [FromBody]Review review)
-        {
-			if(review.Id != id)
-			{
-				return BadRequest();
-			}
+ //       // PUT api/values/5
+ //       [HttpPut("{id}")]
+ //       public IActionResult Put(int id, [FromBody]Review review)
+ //       {
+	//		if(review.Id != id)
+	//		{
+	//			return BadRequest();
+	//		}
 
-			ReviewManager.Save(review);
+	//		ReviewManager.Save(review);
 
-			return AcceptedAtAction(nameof(Get), new { id = review.Id }, review);
-        }
+	//		return AcceptedAtAction(nameof(Get), new { id = review.Id }, review);
+ //       }
 
-		[HttpDelete]
-		public IActionResult Delete()
-		{
-			IEnumerable<Review> Reviews = ReviewManager.GetReviews();
-			foreach(Review Review in Reviews)
-			{
-				ReviewManager.Delete(Review.Id);
-			}
+	//	[HttpDelete]
+	//	public IActionResult Delete()
+	//	{
+	//		IEnumerable<Review> Reviews = ReviewManager.GetReviews();
+	//		foreach(Review Review in Reviews)
+	//		{
+	//			ReviewManager.Delete(Review.Id);
+	//		}
 
-			return Accepted();
-		}
+	//		return Accepted();
+	//	}
 
-		// DELETE api/values/5
-		[HttpDelete("{id}")]
-        public IActionResult Delete(int id)
-        {
-			ReviewManager.Delete(id);
+	//	// DELETE api/values/5
+	//	[HttpDelete("{id}")]
+ //       public IActionResult Delete(int id)
+ //       {
+	//		ReviewManager.Delete(id);
 
-			return AcceptedAtAction(nameof(Get));
-        }
-    }
+	//		return AcceptedAtAction(nameof(Get));
+ //       }
+ //   }
 }
