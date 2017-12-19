ed22aa41cec0c3cef2382a2a1368b761ff4f6b8d
Hugo Zink <hugo.zink@hotmail.com>
2017-12-19 13:59:59 +0100

    Moved YouTube API loading to its own script file.

---
+++ HomeMyDay.Web/wwwroot/js/youtubeapi.js
@@ -0,0 +1,6 @@
+﻿//Load YouTube script API
+//This method is necessary to load the script asynchronously.
+var scriptElement = document.createElement("script");
+scriptElement.src = "http://www.youtube.com/iframe_api";
+var firstScriptElement = document.getElementsByTagName("script")[0];
+firstScriptElement.parentNode.insertBefore(scriptElement, firstScriptElement);
