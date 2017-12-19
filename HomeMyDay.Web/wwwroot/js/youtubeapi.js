510746d6f8c201b977b195e0f67d8b99e1a61e01
Hugo Zink <hugo.zink@hotmail.com>
2017-12-19 14:12:42 +0100

    Changed var to let in YouTube API.

--- HomeMyDay.Web/wwwroot/js/youtubeapi.js
+++ HomeMyDay.Web/wwwroot/js/youtubeapi.js
@@ -1,8 +1,8 @@
 ﻿//Load YouTube script API
 //This method is necessary to load the script asynchronously.
-var scriptElement = document.createElement("script");
+let scriptElement = document.createElement("script");
 scriptElement.src = "http://www.youtube.com/iframe_api";
-var firstScriptElement = document.getElementsByTagName("script")[0];
+let firstScriptElement = document.getElementsByTagName("script")[0];
 firstScriptElement.parentNode.insertBefore(scriptElement, firstScriptElement);
 
 let callbacks = [];
