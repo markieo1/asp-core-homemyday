c61e219021f01e0626c36508302af38eacff4d10
Daniel van Bavel <dvanbavel@gmail.com>
2017-12-20 09:36:31 +0100

    edited for rss feed

--- HomeMyDay.Web.Site.Home/Controllers/PressController.cs
+++ HomeMyDay.Web.Site.Home/Controllers/PressController.cs
@@ -1,11 +1,41 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using System.Text;
+using System.Xml;
 
 namespace HomeMyDay.Web.Site.Home.Controllers
 {
 	public class PressController : Controller
     {
+        private string xml;
+
         public ViewResult Index()
         {
+            xml = "https://www.nu.nl/rss/Algemeen";
+
+            XmlReader reader = XmlReader.Create(new StringReader(xml));
+
+            
+            int startIndex = xml.IndexOf('<');
+            if (startIndex > 0)
+            {
+                xml = xml.Remove(0, startIndex);
+            
+
+                while (reader.Read())
+                {
+                    if ((reader.NodeType == XmlNodeType.Element) && (reader.Name == "item"))
+                    {
+                        if (reader.HasAttributes)
+                        {
+                            reader.GetAttribute("title");
+                            reader.GetAttribute("description");
+                        }                        
+                    }
+                }
+            }
+            ViewData["reader"] = reader;
             return View();
         }
     }
