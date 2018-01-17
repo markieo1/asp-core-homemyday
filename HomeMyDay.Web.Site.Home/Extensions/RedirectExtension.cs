b2f7573a5d05084400eb1f6f854d07657ab49dd3
Kevin Ly <kevinly92@hotmail.com>
2018-01-17 21:43:53 +0100

    Fix for Accommodation. All redirect to Angular App. Replaced Extension to base for multiple use.

--- HomeMyDay.Web.Site.Home/Extensions/RedirectExtension.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace HomeMyDay.Web.Site.Home.Extensions
-{
-    public class RedirectExtension
-    {
-        public string SpaIp { get; set; }
-		public string NodeIp { get; set; }
-    }
-}
