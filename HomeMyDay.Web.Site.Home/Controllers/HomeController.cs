950ee5d333996ac4e852c71b5333fff9c4c475e0
Daniel van Bavel <dvanbavel@gmail.com>
2018-01-17 11:41:16 +0100

    redirect value from appsettings

--- HomeMyDay.Web.Site.Home/Controllers/HomeController.cs
+++ HomeMyDay.Web.Site.Home/Controllers/HomeController.cs
@@ -1,19 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
 
 namespace HomeMyDay.Web.Site.Home.Controllers
 {
 	public class HomeController : Controller
     {
-        private readonly IConfiguration _iconfiguration;
-        public HomeController(IConfiguration iconfiguration)
-        {
-            _iconfiguration = iconfiguration;
-        }
-
         public IActionResult Index()
         {
-            ViewData["url"] = _iconfiguration.GetSection("ExternalAddresses").GetSection("SPA-IP").Value;
             return View();
         }
     }
---
+++ HomeMyDay.Web.Site.Home/Extensions/RedirectExtension.cs
@@ -0,0 +1,7 @@
+﻿namespace HomeMyDay.Web.Site.Home.Extensions
+{
+    public class RedirectExtension
+    {
+        public string SpaIp { get; set; }
+    }
+}
