871674c9265a603d0824e906ef6f9b0286ab4741
Kevin Ly <kevinly92@hotmail.com>
2018-01-17 18:33:33 +0100

    Removed unused imports

--- HomeMyDay.Infrastructure/Repository/MDBAccommodationRepository.cs
+++ HomeMyDay.Infrastructure/Repository/MDBAccommodationRepository.cs
@@ -2,14 +2,11 @@
 using HomeMyDay.Core.Repository;
 using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace HomeMyDay.Infrastructure.Repository
